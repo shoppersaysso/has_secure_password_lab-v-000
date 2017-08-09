e8a1508207e760daf41abc1a738df62ca338f637
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 17:49:12 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params).save
+  end
+
+  def show
+    user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+end
