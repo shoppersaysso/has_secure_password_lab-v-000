cac5d2adbabbb94ec6496f169f4a39bf01512824
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 17:50:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,13 @@
-class UsersController < ApplicationController
+class SessionssController < ApplicationController
 
   def new
   end
 
   def create
-    user = User.new(user_params).save
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
   end
-
-  def show
-    user = User.find(params[:id])
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 
 end
