724a47d3976ce03dcb97407aa56dc4d69be2dd5d
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 18:27:54 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:user][:name])
-    user = user.try(:authenticate, params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
       session[:user_id] = @user.id
   end
 
