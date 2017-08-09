78ff8a927f7aa248fbee487358b87cec7fb4fb67
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 18:20:32 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates :username, presence: true
-    validates :name, presence: true
-    validates :password, presence: true
-    validates :password_confirmation, presence: true
-
-    def self.valid?
-      if params[:password] == params[:password_confirmation]
-    end
+  
 
 end
