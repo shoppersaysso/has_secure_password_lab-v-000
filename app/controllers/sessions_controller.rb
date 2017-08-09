f83898bf53c53a5bc258529ebb5a48ccea7d41c5
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 18:25:31 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(name: params[:username])
     return head(:forbidden) unless @user.authenticate(params[:password])
       session[:user_id] = @user.id
   end
+
+
 end
