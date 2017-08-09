0d28aab8fbdef63dbd71c1734b21842b6e9a9b33
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 17:41:05 +0000

    automatically backed up by learn

--- db/migrate/20170809174009_create_users.rb
+++ db/migrate/20170809174009_create_users.rb
@@ -2,6 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
 
+      t.string :username
+      t.string :password
+      
       t.timestamps null: false
     end
   end
