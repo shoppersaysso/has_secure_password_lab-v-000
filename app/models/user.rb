6789bb7c1a8cc2fba069af330f98f588497569f0
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 18:18:15 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     validates :password_confirmation, presence: true
 
     def is_valid?
-
+      if params[:password] == params[:password_confirmation]
     end
 
 end
