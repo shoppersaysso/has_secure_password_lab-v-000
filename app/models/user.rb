ea69a01b04ed2b0fa6d1d10011bee7cd079a150c
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 18:10:14 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates :name, presence: true
+    validates :username, presence: true
     validates :password, presence: true
     validates :password_confirmation, presence: true
 
